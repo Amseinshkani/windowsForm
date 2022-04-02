4e3d74a64f3420ab23c6a6b23cc7d7c191672a8b
Amseinshkani <amirashkani10@gmail.com>
2022-04-02 20:11:17 +0430

    windows form

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Submit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.Lbllabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Submit.Location = new System.Drawing.Point(265, 115);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(149, 39);
+            this.Submit.TabIndex = 0;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("B Badr", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(91, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Say your name so I can greet you";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(91, 71);
+            this.TxtName.Multiline = true;
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(323, 38);
+            this.TxtName.TabIndex = 2;
+            this.TxtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Lbllabel
+            // 
+            this.Lbllabel.AutoSize = true;
+            this.Lbllabel.Font = new System.Drawing.Font("B Badr", 9F);
+            this.Lbllabel.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.Lbllabel.Location = new System.Drawing.Point(67, 123);
+            this.Lbllabel.Name = "Lbllabel";
+            this.Lbllabel.Size = new System.Drawing.Size(0, 27);
+            this.Lbllabel.TabIndex = 3;
+            this.Lbllabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 193);
+            this.Controls.Add(this.Lbllabel);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Submit);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello MySelf";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label Lbllabel;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            if (TxtName.Text != "")
+            {
+                Lbllabel.ForeColor = Color.MediumSeaGreen;
+                Lbllabel.Text = " Hello " + TxtName.Text;
+            }
+            else
+            {
+                Lbllabel.ForeColor = Color.Red;
+                Lbllabel.Text = " enter your name foolish ";
+            }
+        }
+    }
+}
